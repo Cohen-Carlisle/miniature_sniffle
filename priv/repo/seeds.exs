440b070bf2ad9cac192ae3b6c92f364a92b942c6
Cohen Carlisle <cohencarlisle+github@protonmail.com>
2019-02-10 22:22:45 -0500

    add seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,40 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias MiniatureSniffle.Repo
+alias MiniatureSniffle.{Account, Requisition}
+
+pharmacy1 =
+  %Account.Pharmacy{}
+  |> Account.Pharmacy.changeset(%{name: "Alfa Pharmacy"})
+  |> Repo.insert!()
+
+pharmacy2 =
+  %Account.Pharmacy{}
+  |> Account.Pharmacy.changeset(%{name: "Bravo Pharmacy"})
+  |> Repo.insert!()
+
+%Requisition.Location{}
+|> Requisition.Location.changeset(%{latitude: "39.9612", longitude: "82.9988", pharmacy_id: pharmacy1.id})
+|> Repo.insert!()
+
+%Requisition.Location{}
+|> Requisition.Location.changeset(%{latitude: "40.9612", longitude: "72.9988", pharmacy_id: pharmacy2.id})
+|> Repo.insert!()
+
+%Requisition.Patient{}
+|> Requisition.Patient.changeset(%{first_name: "First", last_name: "User"})
+|> Repo.insert!()
+
+%Requisition.Patient{}
+|> Requisition.Patient.changeset(%{first_name: "Second", last_name: "User"})
+|> Repo.insert!()
+
+%Requisition.Prescription{}
+|> Requisition.Prescription.changeset(%{name: "Allegra"})
+|> Repo.insert!()
+
+%Requisition.Prescription{}
+|> Requisition.Prescription.changeset(%{name: "Rolaids"})
+|> Repo.insert!()
